e3967384463892336c37f31404a6fad2a2c8601c
hussamalbahadli <hussamalbahadli@gmail.cmo>
2018-10-11 18:50:18 +0300

    تصحيح الكود لسماح بدخول وحروج الطالب اكثر من مرة  -- allow inside & out

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,59 @@
-#include <Arduino.h>
 
-#include <SPI.h>
-#include <MFRC522.h>
+#include <Arduino.h> // مكتبة الاردوينو الاساسية 
+#include <SPI.h> // مكتبة تسمح لي بالاتصال باي جهاز يعمل على هذا البروتوكول 
+#include <MFRC522.h> // مكتبة الخاصة بال rfid reader -- rc522
 
-#define SS_PIN 10 //RX slave select
-#define RST_PIN 9
+#define SS_PIN 10 //منفذ دخول البيانات 
+#define RST_PIN 9 // منفذ خروج البيانات
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
+MFRC522 mfrc522(SS_PIN, RST_PIN); // أنشاء الاتصال 
 
-byte card_ID[4];                          //card UID size 4byte
-byte Name1[4] = {0x44, 0x1A, 0xA3, 0x39}; //first UID card
-byte Name2[4] = {0x72, 0x02, 0xDE, 0x24}; //second UID card
+byte card_ID[4]; //حجم البطاقة هو 4 بايت 
+byte Name1[4] = {0x44, 0x1A, 0xA3, 0x39}; //البطاقة الاولى 
+byte Name2[4] = {0x72, 0x02, 0xDE, 0x24}; //البطاقة الثانية 
 
-int NumbCard[2]; //the number of cards. in my case i have just two cards.
+// ملاحظة يستخدم هذا في حالة دخول وخروج الطالب مرة واحدة فقط فهو اختياري 
+int NumbCard[2]; //عدد البطاقات . والتي هي 2 فقط 
+int statu[2]; // عدد البطاقات . والتي هي 2 ققط
+
+int s = 0; 
 int j = 0;
-int statu[2]; //the number of cards. in my case i have just two cards.
-int s = 0;
 
-int const RedLed = 6;
-int const GreenLed = 5;
-int const Buzzer = 8;
+int const RedLed = 6; //  ضوء احمر 
+int const GreenLed = 5; // ضوء احضر 
+int const Buzzer = 8; // جرص 
 
-String Log;
-//String Name;//user name
+//String Log;
 long ID; //user number
-int n;   //The number of card you want to detect (optional)
 
 void setup()
 {
 
-  Serial.begin(9600); // Initialize serial communications with the PC
-  SPI.begin();        // Init SPI bus
-  mfrc522.PCD_Init(); // Init MFRC522 card
+  Serial.begin(9600); // انشاء اتصال بين المتحكم والحاسوب 
+  SPI.begin();        // انشاء بروتوكول 
+  mfrc522.PCD_Init(); // تعريف المكتبة 
 
-  //Serial.println("CLEARSHEET");                 // clears starting at row 1
-  Serial.println("LABEL,Date,ID,Time IN,Time OUT"); // make four columns (Date,Time,[Name:"user name"]line 48 & 52,[Number:"user number"]line 49 & 53)
+  //Serial.println("CLEARSHEET"); // هذا الامر يمسح البيانات في كل مرة يشتغل البرنامج 
+  Serial.println("LABEL,Date,ID,Time IN,Time OUT"); // انشاء جدول يحتوي على التاريخ و رقم الطالب و وقت الدخول و وقت الحروج 
+   // ملاحظة اسم الطالب في برنامج الاكسل 
+  pinMode(RedLed, OUTPUT); // تهيئة المنفذ خروج 5 فولت 
+  pinMode(GreenLed, OUTPUT); // تهيئة المنفذ خروج 5 فولت 
+  pinMode(Buzzer, OUTPUT); // تهيئة المنفذ خروج 5 فولت 
 
-  pinMode(RedLed, OUTPUT);
-  pinMode(GreenLed, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
-
-  delay(200);
+  delay(200); // انتظار 200 ملي ثانية 
 }
 
 void loop()
 {
-  //look for new card
+  //بحث عن بطاقات 
   if (!mfrc522.PICC_IsNewCardPresent())
   {
-    return; //got to start of loop if there is no card present
+    return; //اذا لم تعثر على بطاقات اعد البحث 
   }
-  // Select one of the cards
+  // تحديد البطاقات
   if (!mfrc522.PICC_ReadCardSerial())
   {
-    return; //if read card serial(0) returns 1, the uid struct contians the ID of the read card.
+    return; //اذا ودظت بطاقة ارسلها
   }
 
   for (byte i = 0; i < mfrc522.uid.size; i++)
@@ -62,15 +62,14 @@ void loop()
 
     if (card_ID[i] == Name1[i])
     {
-      //Name ="";//user name
-      ID = 1; //user number
+      
+      ID = 1; // رقم الطالب
       j = 0;
       s = 0;
     }
     else if (card_ID[i] == Name2[i])
     {
-      //Name ="";//user name
-      ID = 2; //user number
+      ID = 2; //رقم الطالب
       j = 1;
       s = 1;
     }
@@ -78,16 +77,14 @@ void loop()
     {
       digitalWrite(GreenLed, LOW);
       digitalWrite(RedLed, HIGH);
-      goto cont; //go directly to line 71
+      goto cont; //شغل ضوء احمر اذا لم تعثر على شيء 
     }
   }
   if (NumbCard[j] == 1 && statu[s] == 0)
   {
-    statu[s] = 1;
-    Serial.print("DATA,DATE,"); //send the Name to excel
+    //statu[s] = 1; // عطلت هذا الامر لكي يسمح لكل طالب بالدخول والخروج اكثر من مرة في الصف 
+    Serial.print("DATA,DATE,"); //ارسل البيانات الى برنامج الاكسل
     Serial.print(ID);
-    // Serial.print(",");
-    // Serial.print(Name); //send the Number to excel
     Serial.print(",");
     Serial.print("");
     Serial.print(",");
@@ -95,12 +92,9 @@ void loop()
   }
   else if (NumbCard[j] == 0)
   {
-    NumbCard[j] = 1;
-    n++;
-    Serial.print("DATA,DATE,"); //send the Name to excel
+   // NumbCard[j] = 1; // عطلت هذا الامر لكي يسمح لكل طالب بالدخول والخروج اكثر من مرة في الصف 
+    Serial.print("DATA,DATE,"); //ارسل البيانات الى برنامج الاكسل
     Serial.print(ID);
-    // Serial.print(",");
-    // Serial.print(Name); //send the Number to excel
     Serial.print(",");
     Serial.print("TIME");
     Serial.print(",");
@@ -113,7 +107,7 @@ void loop()
   }
   else if (statu[s] == 1)
   {
-    //Turn Red LED when the employee Already Left
+    //شغل الضوء الاحمر اذا خرج الطالب من الصف 
     digitalWrite(RedLed, HIGH);
   }
   delay(1000);
@@ -122,7 +116,7 @@ cont:
   digitalWrite(GreenLed, LOW);
   digitalWrite(RedLed, LOW);
 
-  //if you want to close the Excel when all card had detected and save Excel file in Names Folder. in my case i have just 2 card (optional)
+  // اوامر حفظ اختياري
   /*if(n==2){
     Serial.println("SAVEWORKBOOKAS,Names/WorkNames");
     Serial.println("FORCEEXCELQUIT");
